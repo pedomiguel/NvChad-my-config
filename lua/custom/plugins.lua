463a9cf35a9277cdb8b0e5aabf31e7dc27ce9b62
synthbe <pedrom.varelac@gmail.com>
2024-02-29 13:15:09 -0300

    add: custom

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,85 @@
+local plugins = {
+  {
+    "mfussenegger/nvim-jdtls",
+    ft = "java",
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  {
+    "mfussenegger/nvim-dap", -- Debugging
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {}
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  -- {
+  --   "mfussenegger/nvim-dap-python",
+  --   ft = "python",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     "rcarriga/nvim-dap-ui",
+  --   },
+  --   config = function(_, opts)
+  --     local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+  --     require("dap-python").setup(path)
+  --     require("core.utils").load_mappings("dap_python")
+  --   end,
+  -- },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function ()
+        require "custom.configs.null-ls"
+      end,
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+        -- "debugpy",
+        "clangd",
+        "codelldb",
+        "html-lsp",
+        "jdtls",
+      },
+    },
+  },
+}
+return plugins
