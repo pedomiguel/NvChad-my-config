ef7fa3ba98d4cb89dd77314db10075b72abd32dd
synthbe <pedrom.varelac@gmail.com>
2024-05-17 18:50:52 -0300

    typo correction tagbar config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -6,7 +6,7 @@ local plugins = {
       vim.g.tagbar_width = math.max(30, vim.api.nvim_win_get_width(0) / 5)
       vim.g.tagbar_show_tag_linenumbers = 1
       vim.g.tagbar_no_status_line = 1
-      vim.g.tabar_sort = 0
+      vim.g.tagbar_sort = 0
     end
   },
   {
