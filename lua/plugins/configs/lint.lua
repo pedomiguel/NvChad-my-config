878b805a02547348ef31b399b7aad192dd6e07b4
synthbe <108361383+synthbe@users.noreply.github.com>
2024-12-01 21:37:05 -0300

    fix: linting table

--- lua/plugins/configs/lint.lua
+++ lua/plugins/configs/lint.lua
@@ -1,7 +1,7 @@
 local lint = require("lint")
 
 lint.linters_by_ft = {
-  python = { "mypy", },
+  python = { "mypy" },
 }
 
 local mypy = lint.linters.mypy
@@ -11,13 +11,9 @@ mypy.args = {
   '--show-error-end',
   '--no-site-packages',
   '--ignore-missing-imports',
-  '--hide-error-codes',
-  '--hide-error-context',
   '--no-color-output',
-  '--no-error-summary',
-  '--no-pretty',
   '--python-executable',
   function()
-    return vim.fn.exepath 'python3' or vim.fn.exepath 'python'
+    return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python3'
   end
 }
